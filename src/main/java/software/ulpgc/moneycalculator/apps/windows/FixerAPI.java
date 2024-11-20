9bd2fed34d0e1fa9539ad88a3d2b4734bcbee516
Juanquirr <juanquirr579@gmail.com>
2024-11-20 10:29:10 +0000

    initial files

---
+++ src/main/java/software/ulpgc/moneycalculator/apps/windows/FixerAPI.java
@@ -0,0 +1,5 @@
+package software.ulpgc.moneycalculator.apps.windows;
+
+public class FixerAPI {
+    public static final String key = "31547d8fcf138f06d26b4f66076050d1";
+}
---
+++ src/main/java/software/ulpgc/moneycalculator/io/MockMain.java
@@ -0,0 +1,16 @@
+package software.ulpgc.moneycalculator.io;
+
+import java.util.Currency;
+import java.util.List;
+
+public class MockMain {
+    public static void main(String[] args) {
+        List<Currency> currencies = new MockCurrencyLoader().load();
+        MoneyDialog moneyDialog = new MockMoneyDialog().define(currencies);
+        CurrencyDialog currencyDialog = new MockCurrencyDialog().define(currencies);
+        MoneyDisplay moneyDisplay = new MockMoneyDisplay();
+        ExchangeRateLoader exchangeRateLoader = new MockExchangeRateLoader();
+        Command command = new ExchangeMoneyCommand(moneyDialog, currencyDialog, exchangeRateLoader, moneyDisplay);
+        command.execute();
+    }
+}
